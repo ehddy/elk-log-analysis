14e8ea9d0941b80e9fb3c4ae85fcc6f8c6c4cc4b
ehddy <rlxk751@naver.com>
2023-07-10 17:58:29 +0900

    자동 log 삭제 sh 파일 추가

---
+++ shell_scripts/log_auto_delete.sh
@@ -0,0 +1,4 @@
+# 3일 이상된 로그 파일 삭제
+find /code/logs/save_elasticsearch/ -type f -name "save_program_*.log" -mtime +1 -exec rm {} \;
+find /code/logs/model_result/ -type f -name "elastic_program_*.log" -mmin +0.5 -exec rm {} \;
+
